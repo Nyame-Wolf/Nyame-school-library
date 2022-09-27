e5f0d74dac107c5d5fb4ecfd3ea051f0c8987949
Nyame-Wolf <mumenyam@gmail.com>
2022-09-27 17:57:20 +0300

    add a student and teacher classes

---
+++ teacher.rb
@@ -0,0 +1,12 @@
+require 'person'
+
+class Person < Teacher
+  def initialize(*args, specialization)
+    super(*args)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
