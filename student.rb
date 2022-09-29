d8d6aa4c58d6104f1d5fe4d6e00110866c0b3216
Nyame-Wolf <mumenyam@gmail.com>
2022-09-29 08:33:57 +0300

    create a classroom class that has a has many to students.
     create a belongs to class where student belongs to a class

--- student.rb
+++ student.rb
@@ -1,12 +1,19 @@
 require 'person'
 
-class Person < Student
+class Student < Person
+  attr_accessor :classroom
+
   def initialize(*args, classroom)
     super(*args)
     @classroom = classroom
+    update_classroom(@classroom)
   end
 
   def play_hooky
     "¯\(ツ)/¯"
+  end
+
+  def update_classroom(classroom)
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
