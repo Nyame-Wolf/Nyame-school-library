725d2d7f6d2a8d3415034a4716bb816ef012f3ec
Nyame-Wolf <mumenyam@gmail.com>
2022-09-29 08:37:19 +0300

    create book class
    create an intermediae class rental
    
    create a many to many relationship beween book and person through rental

--- person.rb
+++ person.rb
@@ -10,8 +10,9 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
-  attr_reader :id
+  attr_reader :id, :rentals
   attr_accessor :name, :age
 
   def can_use_services?
