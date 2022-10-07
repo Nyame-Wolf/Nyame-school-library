0f232fe3d9064e30c3c0bbef382756eee97d3878
Nyame-Wolf <mumenyam@gmail.com>
2022-10-07 10:20:39 +0300

    fix rental and teacher files

--- teacher.rb
+++ teacher.rb
@@ -1,12 +1,13 @@
 require './person'
 
 class Teacher < Person
-  attr_accessor :specialization
-
-  def initialize(*args, specialization)
-    super(*args)
+  def initialize(specialization, age, name, id)
+    super(name, age)
     @specialization = specialization
+    @id = id || Random.rand(1..1000)
   end
+
+  attr_accessor :specialization
 
   def can_use_services?
     true
